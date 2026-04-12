14cedccefa24c170349cb70570415c4655e6d881
Đặng Trần Anh Hào <63339885+kohrabi@users.noreply.github.com>
2026-04-12 09:00:45 +0700

    test others perks

--- scripts/board/board.gd
+++ scripts/board/board.gd
@@ -80,9 +80,9 @@ func _update_deathline_position() -> void :
 
 func initialize_board() -> void :
 
+	GameManager.activate_perk(GameData.Perks.FULL_CLEAR);
 	GameManager.activate_perk(GameData.Perks.SPEED_RUN);
-	GameManager.activate_perk(GameData.Perks.SACRIFICE_ROW);
-	GameManager.activate_perk(GameData.Perks.COMBO_ENGINE);
+	GameManager.activate_perk(GameData.Perks.AUTOMAGIC);
 
 	placed_blocks_grid.clear()
 	GameManager.clear_placed_blocks_variables()
